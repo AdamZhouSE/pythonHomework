9e16993c90b1da1735c5ae51084395b38d514511
382335657 <382335657@qq.com>
2020-05-19 19:50:50 +0800

    一些整理

---
+++ Code/code_download.py
@@ -0,0 +1,99 @@
+import json
+import urllib.request
+import urllib.parse
+import os
+import zipfile as z
+import requests
+
+
+def callbackfunc(blocknum, blocksize, totalsize):
+    percent = 100 * blocknum * blocksize / totalsize
+    if percent > 100:
+        percent = 100
+    print("%.2f%%" % percent)
+
+
+def delete(haveSorted, filename):
+    with open(filename, 'r', encoding='utf-8') as f:
+        res = f.read()
+        data = json.loads(res)
+        for code_id in haveSorted:
+            if code_id in data.keys():
+                del data[code_id]
+
+    with open(filename, 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+
+def upzip(zip_src, dst_dir):
+    fz = z.ZipFile(zip_src, 'r')
+    zip_src = fz.namelist()[0]
+    fz.extract(zip_src, dst_dir)
+    fz = z.ZipFile(zip_src, 'r')
+    fz.extract('.mooctest/answer.py', dst_dir)
+
+
+def DL(data: dict, path):
+    case_count = 0
+    haveSorted = []
+
+    for case_id, details in data.items():
+        if case_id not in haveSorted:
+            records = details["records"]
+            user_count = 0
+            for record in records:
+                user_id = record["user_id"]
+                for up in record["upload_records"]:
+                    upload_id = up["upload_id"]
+                    code_url = up["code_url"]
+                    try:
+                        download(case_id, user_id, upload_id, code_url, path)
+                    except:
+                        continue
+                user_count += 1
+                print(user_id + "------" + str(user_count))
+            case_count += 1
+            print(case_id + "-------------------------" + str(case_count))
+            haveSorted.append(case_id)
+            print(sorted(haveSorted))
+
+
+def download(case_id, user_id, upload_id, url, path):
+    filename = urllib.parse.unquote(os.path.basename(url))
+
+    os.chdir(path)
+    # 创建文件夹
+    if case_id not in os.listdir(path):
+        os.mkdir(case_id)
+    os.chdir(path + "\\" + case_id)
+    path = os.getcwd()
+
+    if user_id not in os.listdir(path):
+        os.mkdir(user_id)
+    os.chdir(path + "\\" + user_id)
+    path = os.getcwd()
+
+    # 下载
+    if str(upload_id) not in os.listdir(path):
+        #urllib.request.urlretrieve(url, filename, callbackfunc)
+        r = requests.get(url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        upzip(filename, os.getcwd())
+        os.rename(".mooctest", str(upload_id))
+        for file in os.listdir(path):
+            if file.endswith(".zip"):
+                os.remove(file)
+
+
+if __name__ == '__main__':
+    filename = 'data/test_result.json'
+    haveSorted = []
+    delete(haveSorted, filename)
+
+    with open(filename, 'r', encoding='utf-8') as f:
+        res = f.read()
+        data = json.loads(res)
+        print(len(data.keys()))
+        os.chdir(os.getcwd() + "\\Code_Records")
+        DL(data, os.getcwd())
---
+++ Code/detect_if_else_structure.py
@@ -0,0 +1,60 @@
+"""
+检测面向用例情况
+if-elif-else结构（包括内部的内容）占该题总行数的40%
+"""
+
+
+def cal_if_else_structure(file):  # 统计if-else结构的数量
+    with open(file) as fp:
+        content_list = fp.readlines()
+        # print(content_list)
+        content_list_code = []  # 取出所有的有效代码行
+        file_code_num = 0
+        file_if_else_num = 0
+        # 统计代码行数
+        index = 0
+        for content in content_list:
+            content = content.strip()
+            if content != '' and not content.startswith("#"):
+                file_code_num += 1
+                content_list_code.append(content_list[index])
+            index += 1
+        length = len(content_list_code)
+        # 统计if-else结构
+        for i in range(0, length):
+            if "if" in content_list_code[i] or "elif" in content_list_code[i] or "else" in content_list_code[i]:
+                file_if_else_num += 1
+                count_blank = 0  # 统计句首的空格数
+                # print(content_list_code[i])
+                for j in range(0, len(content_list_code[i])):
+                    if content_list_code[i][j] == " ":
+                        count_blank += 1
+                    else:
+                        break
+                print("if-else句首空格", count_blank)
+                # 统计内部内容，句首空格数大于if-else结构的句首空格数
+                for j in range(i+1, length):
+                    count_in_blank = 0
+                    for k in range(0, len(content_list_code[j])):
+                        if content_list_code[j][k] == " ":
+                            count_in_blank += 1
+                        else:
+                            break
+                    print("内部空格", count_in_blank)
+                    if count_in_blank > count_blank:
+                        file_if_else_num += 1
+                    else:
+                        break
+        print(file_code_num, file_if_else_num)
+        per = file_if_else_num/file_code_num
+        if per > 0.4:
+            return True, per
+        else:
+            return False, per
+
+
+if __name__ == "__main__":
+    res, percent = cal_if_else_structure("Code/test_if_else.py")
+    print(res, percent)
+
+
